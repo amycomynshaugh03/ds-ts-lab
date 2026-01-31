7d9f15a1afbef1cead9488a54f92f3cc00925ed8
Amy Comyns Haugh <comynsamy03@gmail.com>
2026-01-31 14:22:15 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,27 +1,28 @@
-import {Friend, Colleague } from './myTypes'
+import { Friend, Colleague, EmailContact } from './myTypes';
 import { friends } from './01-basics';
 import { colleagues } from './01-basics';
 
-function older(f: Friend) : string {
+function older(f: Friend) {
      f.age += 1
      return `${f.name} is now ${f.age}` 
 }
 
 console.log(older(friends[0]))
 
-function allOlder(friends: Friend[]): string[] {
+function allOlder(friends: Friend[]) {
   return friends.map(f => older(f));
 }
 
 console.log(allOlder(friends));
 
 
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) { // Inferred retun type
   const result = cs.sort(
     (c1, c2) => c1.contact.extension - c2.contact.extension
   );
   return result[cs.length - 1];
 }
+
 console.log(highestExtension(colleagues.current));
 
 function addColleague(cs: Colleague[], newColleague: Colleague): Colleague[] {
@@ -43,3 +44,27 @@ const newColleague: Colleague = {
 };
 
 console.log(addColleague(colleagues.current, newColleague));
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+function findFriends(
+  friends: Friend[],
+  predicate: (friend: Friend) => boolean
+): string[] {
+  return friends
+    .filter(predicate)
+    .map(friend => friend.name);
+}
+
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
